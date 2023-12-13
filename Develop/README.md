f2ac5394430beb202400d0c0997302c000de51ec
jessicanwey <jessica.n.wey@gmail.com>
2023-12-13 11:51:23 -0600

    updated to correct syntax for generating license links

--- Develop/README.md
+++ Develop/README.md
@@ -1,10 +1,10 @@
-# The Best Project
+# k
 
   ## Badges
-  [![License](https://img.shields.io/badge/License-Artistic-2.0-blue.svg)](https://opensource.org/license/artistic-2-0/)
+  [![License](https://img.shields.io/badge/License-BSL--2--Clause-blue.svg)](https://opensource.org/license/bsdpluspatent/)
 
   ## Description
-  It does the best things for the best people.
+  k
     
   ## Table of Contents
   * [Installation](#installation)
@@ -15,56 +15,21 @@
   * [Questions](#questions)
 
   ## Installation
-  Install it 
+  k
 
   ## Usage
-  Please do
+  k
 
   ## License
-  Artistic license 2.0  
-  https://opensource.org/license/artistic-2-0/
-
-  ## Contributing
-  Help!
-
-  ## Tests
-  Try it
-
-  ## Questions
-  GitHub: [jessicanwey](https://github.com/jessicanwey)  
-  Please contact me via email with additional questions: jessica.n.wey@gmail.com
-# sdfsdf
-
-  ## Badges
-  [![License](https://img.shields.io/badge/License-0BSD-blue.svg)](https://opensource.org/license/bsdpluspatent/)
-
-  ## Description
-  sdfsdf
-    
-  ## Table of Contents
-  * [Installation](#installation)
-  * [Usage](#usage)
-  * [License](#license)
-  * [Contributing](#contributing)
-  * [Tests](#tests)
-  * [Questions](#questions)
-
-  ## Installation
-  sdfsdf
-
-  ## Usage
-  sdfsdf
-
-  ## License
-  BSD Zero-Clause license  
+  BSD 2-clause "Simplified" license  
   https://opensource.org/license/bsdpluspatent/
 
   ## Contributing
-  sdfsdf
+  k
 
   ## Tests
-  sdfsdf
+  k
 
   ## Questions
-  GitHub: [sdfsdf](https://github.com/sdfsdf)  
-  Please contact me via email with additional questions: sdfsdf
+  GitHub: [k](https://github.com/k)  
+  Please contact me via email with additional questions: k
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -4,21 +4,21 @@ function renderLicenseBadge(license) {
   let licenseBadge = '';
   console.log(license);
         if(license === 'Academic Free License v3.0') {
-          licenseBadge = '[![License](https://img.shields.io/badge/License-AFL-3.0-blue.svg)](https://opensource.org/license/afl-3-0-php/)';
+          licenseBadge = '[![License](https://img.shields.io/badge/License-AFL--3.0-blue.svg)](https://opensource.org/license/afl-3-0-php/)';
         }if(license === 'Apache license 2.0'){
-          licenseBadge = '[![License](https://img.shields.io/badge/License-Apache-2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+          licenseBadge = '[![License](https://img.shields.io/badge/License-Apache--2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
         }if(license === 'Artistic license 2.0'){
-          licenseBadge = '[![License](https://img.shields.io/badge/License-Artistic-2.0-blue.svg)](https://opensource.org/license/artistic-2-0/)';
+          licenseBadge = '[![License](https://img.shields.io/badge/License-Artistic--2.0-blue.svg)](https://opensource.org/license/artistic-2-0/)';
         }if(license === 'Boost Software License 1.0'){
-          licenseBadge = '[![License](https://img.shields.io/badge/License-BSL-1.0-blue.svg)](https://opensource.org/license/bsl-1-0/)';
+          licenseBadge = '[![License](https://img.shields.io/badge/License-BSL--1.0-blue.svg)](https://opensource.org/license/bsl-1-0/)';
         }if(license === 'BSD 2-clause "Simplified" license'){
-          licenseBadge = '[![License](https://img.shields.io/badge/License-BSL-2-Clause-blue.svg)](https://opensource.org/license/bsdpluspatent/)';
+          licenseBadge = '[![License](https://img.shields.io/badge/License-BSL--2--Clause-blue.svg)](https://opensource.org/license/bsdpluspatent/)';
         }if(license === 'BSD 3-clause "New" or "Revised" license'){
-          licenseBadge = '[![License](https://img.shields.io/badge/License-BSL-3-Clause-blue.svg)](https://opensource.org/license/bsdpluspatent/)';
+          licenseBadge = '[![License](https://img.shields.io/badge/License-BSL--3--Clause-blue.svg)](https://opensource.org/license/bsdpluspatent/)';
         }if(license === 'BSD 3-clause Clear license'){
-          licenseBadge = '[![License](https://img.shields.io/badge/License-BSL-3-Clause-Clear-blue.svg)](https://opensource.org/license/bsdpluspatent/)';
+          licenseBadge = '[![License](https://img.shields.io/badge/License-BSL--3--Clause--Clear-blue.svg)](https://opensource.org/license/bsdpluspatent/)';
         }if(license === 'BSD 4-clause "Original" or "Old" license'){
-          licenseBadge = '[![License](https://img.shields.io/badge/License-BSL-4-Clause-blue.svg)](https://opensource.org/license/bsdpluspatent/)';
+          licenseBadge = '[![License](https://img.shields.io/badge/License-BSL--4--Clause-blue.svg)](https://opensource.org/license/bsdpluspatent/)';
         }if(license === 'BSD Zero-Clause license'){
           licenseBadge = '[![License](https://img.shields.io/badge/License-0BSD-blue.svg)](https://opensource.org/license/bsdpluspatent/)';
         }if(license === 'Creative Commons license family'){
@@ -26,33 +26,33 @@ function renderLicenseBadge(license) {
         }if(license === 'Do What The F*ck You Want To Public License'){
           licenseBadge = '[![License](https://img.shields.io/badge/License-WTFPL-blue.svg)]';
         }if(license === 'Educational Community License v2.0'){
-          licenseBadge = '[![License](https://img.shields.io/badge/License-ECL-2.0-blue.svg)](https://opensource.org/license/ecl-2-0/)';
+          licenseBadge = '[![License](https://img.shields.io/badge/License-ECL--2.0-blue.svg)](https://opensource.org/license/ecl-2-0/)';
         }if(license === 'Eclipse Public License 1.0'){
-          licenseBadge = '[![License](https://img.shields.io/badge/License-EPL-1.0-blue.svg)](https://opensource.org/license/ecl-1-0/)';
+          licenseBadge = '[![License](https://img.shields.io/badge/License-EPL--1.0-blue.svg)](https://opensource.org/license/ecl-1-0/)';
         }if(license === 'Eclipse Public License 2.0'){
-          licenseBadge = '[![License](https://img.shields.io/badge/License-EPL-2.0-blue.svg)](https://opensource.org/license/ecl-2-0/)';
+          licenseBadge = '[![License](https://img.shields.io/badge/License-EPL--2.0-blue.svg)](https://opensource.org/license/ecl-2-0/)';
         }if(license === 'European Union Public License 1.1'){
-          licenseBadge = '[![License](https://img.shields.io/badge/License-EUPL-1.1-blue.svg)](https://opensource.org/license/eupl-1-2/)';
+          licenseBadge = '[![License](https://img.shields.io/badge/License-EUPL--1.1-blue.svg)](https://opensource.org/license/eupl-1-2/)';
         }if(license === 'GNU Affero General Public License v3.0'){
-          licenseBadge = '[![License](https://img.shields.io/badge/License-AGPL-3.0-blue.svg)](https://opensource.org/license/agpl-v3/)';
+          licenseBadge = '[![License](https://img.shields.io/badge/License-AGPL--3.0-blue.svg)](https://opensource.org/license/agpl-v3/)';
         }if(license === 'GNU Lesser General Public License family'){
-          licenseBadge = '[![License](https://img.shields.io/badge/License-LGPL-blue.svg)](https://opensource.org/license/lgpl-license-html/)';
+          licenseBadge = '[![License](https://img.shields.io/badge/License-LGPL--blue.svg)](https://opensource.org/license/lgpl-license-html/)';
         }if(license === 'GNU Lesser General Public License v2.1'){
-          licenseBadge = '[![License](https://img.shields.io/badge/License-LGPL-2.1-blue.svg)](https://opensource.org/license/lgpl-2-1/)';
+          licenseBadge = '[![License](https://img.shields.io/badge/License-LGPL--2.1-blue.svg)](https://opensource.org/license/lgpl-2-1/)';
         }if(license === 'GNU Lesser General Public License v3.0'){
-          licenseBadge = '[![License](https://img.shields.io/badge/License-LGPL-3.0-blue.svg)](https://choosealicense.com/licenses/agpl-3.0/)';
+          licenseBadge = '[![License](https://img.shields.io/badge/License-LGPL--3.0-blue.svg)](https://choosealicense.com/licenses/agpl-3.0/)';
         }if(license === 'ISC'){
           licenseBadge = '[![License](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/license/isc-license-txt/)';
         }if(license === 'LaTeX Project Public License v1.3c'){
-          licenseBadge = '[![License](https://img.shields.io/badge/License-LPPL-1.3c-blue.svg)](https://opensource.org/license/lppl/)';
+          licenseBadge = '[![License](https://img.shields.io/badge/License-LPPL--1.3c-blue.svg)](https://opensource.org/license/lppl/)';
         }if(license === 'Microsoft Public License'){
-          licenseBadge = '[![License](https://img.shields.io/badge/License-MS-PL-blue.svg)](https://opensource.org/license/ms-pl-html/)';
+          licenseBadge = '[![License](https://img.shields.io/badge/License-MS--PL-blue.svg)](https://opensource.org/license/ms-pl-html/)';
         }if(license === 'Open Software License 3.0'){
-          licenseBadge = '[![License](https://img.shields.io/badge/License-OSL-3.0-blue.svg)](https://opensource.org/license/osl-3-0-php/)';
+          licenseBadge = '[![License](https://img.shields.io/badge/License-OSL--3.0-blue.svg)](https://opensource.org/license/osl-3-0-php/)';
         }if(license === 'PostgreSQL License'){
           licenseBadge = '[![License](https://img.shields.io/badge/License-PostgreSQL-blue.svg)](https://opensource.org/license/postgresql/)';
         }if(license === 'SIL Open Font License 1.1'){
-          licenseBadge = '[![License](https://img.shields.io/badge/License-OFL-1.1-blue.svg)](https://opensource.org/license/ofl-1-1/)';
+          licenseBadge = '[![License](https://img.shields.io/badge/License-OFL--1.1-blue.svg)](https://opensource.org/license/ofl-1-1/)';
         }if(license === 'University of Illinois/NCSA Open Source License'){
           licenseBadge = '[![License](https://img.shields.io/badge/License-NCSA-blue.svg)](https://opensource.org/license/uoi-ncsa-php/)';
         }if(license === 'The Unlicense'){
